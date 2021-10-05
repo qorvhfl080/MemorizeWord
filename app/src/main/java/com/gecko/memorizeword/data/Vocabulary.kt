dae31e3cd6f8048a0ab5a4897e5f1700de521b6c
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-10-05 18:05:31 +0900

    데이터 클래스 추가

---
+++ app/src/main/java/com/gecko/memorizeword/data/Vocabulary.kt
@@ -0,0 +1,5 @@
+package com.gecko.memorizeword.data
+
+class Vocabulary {
+    var voca = ArrayList<Word>()
+}
---
+++ app/src/main/java/com/gecko/memorizeword/data/Word.kt
@@ -0,0 +1,4 @@
+package com.gecko.memorizeword.data
+
+class Word(word: String, meaning: String) {
+}
